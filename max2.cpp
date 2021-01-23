d974c9fc3d6b2cf61801cae20045b6dd7a4c672e
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-01-23 19:55:39 +0700

    Create max2.cpp

---
+++ max2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    if (a>b)
+        printf("%d",a);
+    else
+        printf("%d",b);
+
+    return 0;
+}
