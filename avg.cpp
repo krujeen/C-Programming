1441ff05e732785bc8cdfe7f9ad0bb104ecf0d91
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-01-23 19:44:07 +0700

    Create avg.cpp

---
+++ avg.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int a,b,c;
+    float avg;
+    cin>>a>>b>>c;
+
+    avg = (a+b+c)/3;
+
+    cout<<avg;
+
+
+    return 0;
+}
