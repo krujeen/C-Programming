b3972076221fe7bf41f91f6b81dbd8bb45cc2669
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-01-24 20:10:58 +0700

    Create Day02_01_star.cpp

---
+++ Day02_01_star.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int x=1;
+    int i,j;
+
+    for (i=1;i<=n;i++){
+       for (j=1;j<=n;j++){
+            cout<<"*";
+       }
+       cout<<"\n";
+    }
+    return 0;
+}
+
+/*
+5
+*****
+*****
+*****
+*****
+*****
+*/
