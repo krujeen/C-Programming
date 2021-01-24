a37ec4472c1abc857b8f1832e9ab337c4b686b86
KruJeen <44271005+krujeen@users.noreply.github.com>
2021-01-24 20:48:23 +0700

    Update Day02_03_star.cpp

--- Day02_03_star.cpp
+++ Day02_03_star.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 
+int main()
+{
+    int n;
+    cin>>n;
+    int x=1;
+    int i,j;
+
+    for (i=1;i<=n;i++){
+       for (j=1;j<=n-i;j++){
+            cout<<" ";
+       }
+       for (j=1;j<=i;j++){
+            cout<<"*";
+       }
+       cout<<"\n";
+    }
+    return 0;
+}
+
+/*
+5
+    *
+   **
+  ***
+ ****
+*****
+*/
